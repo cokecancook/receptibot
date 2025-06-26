4a7bd657fdf2c5967fd8fa2debbd5c42562c5dce
cokecancook <cokecancook@gmail.com>
2025-06-26 02:31:37 +0200

    removed duplicate agent/agent folder

--- src/agents/agent/agent/state.py
+++
@@ -1,7 +0,0 @@
-from typing import TypedDict, List, Optional
-from langchain_core.messages import BaseMessage
-
-class GraphState(TypedDict):
-    messages: List[BaseMessage]
-    tool_calls: Optional[List]
-    tool_outputs: Optional[List]
