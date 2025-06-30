275c821087fd7d13d1b3246efc0addc5efc34861
fab1an12 <fabianmiulescu@gmail.com>
2025-06-30 19:18:17 +0200

    Refactor database configuration: move DB_CONFIG and DATABASE_URL to metriclogger.py and remove init.py

--- src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'port': 5432,
-    'database': 'agent_metrics_db',
-    'username': 'postgres',
-    'password': 'postgres'
-}
-
-DATABASE_URL = f"postgresql://{DB_CONFIG['username']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['database']}"
--- src/utils/metriclogger.py
+++ src/utils/metriclogger.py
@@ -16,8 +16,15 @@ from sqlalchemy import (
 )
 from sqlalchemy.exc import SQLAlchemyError
 
-from __init__ import DATABASE_URL
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'postgres',
+    'username': 'postgres',
+    'password': 'postgres'
+}
 
+DATABASE_URL = f"postgresql://{DB_CONFIG['username']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['database']}"
 logger = logging.getLogger(__name__)
 
 
